5373660f0ac663f73f72e377b1163bf3c6412815
JReneHS <jhernandezs1509@alumno.ipn.mx>
2020-12-18 13:56:30 -0600

    cambios rene

--- nyancat_prog_dvorak/keymap.c
+++ nyancat_prog_dvorak/keymap.c
@@ -48,6 +48,7 @@ enum {
     TD_QUOT,
     TD_ALT,
     TD_BACKF,
+    TD_SPC,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -59,7 +60,7 @@ TD(TD_CAPLOCK), KC_A,   KC_O,    KC_E,    KC_U,    KC_I,                        
   //---------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     KC_LCTL, TD(TD_QUOT),   KC_Q,    KC_J,    KC_K,    KC_X,                       KC_B,    KC_M,    KC_W,   KC_V,    KC_Z,   KC_RCTL,
   //---------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LGUI,   LOWER, KC_SPC,       KC_ENT, RAISE, TD(TD_ALT)
+                                        KC_LGUI,   LOWER, TD(TD_SPC),   TD(TD_SPC), RAISE, TD(TD_ALT)
                                       //|--------------------------|  |--------------------------|
 
 
@@ -73,7 +74,7 @@ TD(TD_CAPLOCK), KC_A,   KC_O,    KC_E,    KC_U,    KC_I,                        
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     KC_1,    KC_2,    KC_3,  KC_0, TD(TD_BACKF), KC_ESC,                       KC_APP , KC_ASTR, KC_CIRC, KC_EXLM,TD(TD_MINS), KC_RCTL,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LGUI, KC_TRNS, KC_SPC,        KC_ENT, RAISE, TD(TD_ALT)
+                                        KC_LGUI, KC_TRNS, TD(TD_SPC),  TD(TD_SPC), RAISE, TD(TD_ALT)
                                       //|--------------------------|  |--------------------------|
     ),
 
@@ -85,7 +86,7 @@ TD(TD_CAPLOCK), KC_A,   KC_O,    KC_E,    KC_U,    KC_I,                        
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
     RESET,    KC_NO,   KC_NO,   KC_MYCM, KC_CALC, KC_PSCR,                       KC_MPRV, KC_MNXT, KC_MSTP,  KC_NO,  KC_NO,  KC_NO,
   //|--------+--------+--------+--------+--------+--------+--------|  |--------+--------+--------+--------+--------+--------+--------|
-                                        KC_LGUI, LOWER,    KC_SPC,      KC_ENT, KC_TRNS, TD(TD_ALT)
+                                        KC_LGUI, LOWER,  TD(TD_SPC),    TD(TD_SPC), KC_TRNS, TD(TD_ALT)
                                       //|--------------------------|  |--------------------------|
     )
 };
@@ -106,6 +107,7 @@ qk_tap_dance_action_t tap_dance_actions[] = {
     [TD_TAB] = ACTION_TAP_DANCE_DOUBLE(KC_TAB, KC_ESC),
     [TD_CAPLOCK] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
     [TD_ALT] = ACTION_TAP_DANCE_DOUBLE(KC_RALT, KC_LALT),
+    [TD_SPC] = ACTION_TAP_DANCE_DOUBLE(KC_SPC, KC_ENT),
 };
 
 int RGB_current_mode;
@@ -256,7 +258,7 @@ void render_logo(void) {
         0xa0, 0xa1, 0xa2, 0xa3, 0xa4,
         0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0};
     oled_write_P(corne_logo, false);
-    oled_write_P(PSTR("gio.k"), false);
+    oled_write_P(PSTR("Gio*K"), false);
 }
 
 void render_logo2(void) {
